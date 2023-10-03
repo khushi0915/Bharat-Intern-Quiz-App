cf02a31f5ef287edeb7df6a266f10469bf0b79e5
khushi0915 <146006774+khushi0915@users.noreply.github.com>
2023-10-03 15:09:35 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+       public static String question[] ={
+                "Which company owns the android?",
+                "Which one is not the programming language?",
+                "Where you are watching this video?",
+                "Which company owns the Apple?",
+               "Which is called the power house of cell?"
+        };
+
+        public static String choices[][] = {
+                {"Google","Apple","Nokia","Samsung"},
+                {"Java","Kotlin","Notepad","Python"},
+                {"Facebook","Whatsapp","Instagram","Youtube"},
+                {"Google","Apple","Nokia","Samsung"},
+                {"Cytoplasm","Mitochondria","Ribosomes","Lyzozomes"}
+        };
+
+        public static String correctAnswers[] = {
+                "Google",
+                "Notepad",
+                "Youtube",
+                "Apple",
+                "Mitochondria"
+        };
+
+    }
+
